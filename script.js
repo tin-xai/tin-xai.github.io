ab455dc05f88a64f7af070b57f32f6b8c05a4306
tin <ngthanhtinqn@gmail.com>
2025-10-12 11:22:41 -0500

    update

---
+++ script.js
@@ -0,0 +1,12 @@
+// Hover effects for paper images
+function hypernerf_start() {
+    document.getElementById('hypernerf_image').style.opacity = "1";
+}
+
+function hypernerf_stop() {
+    document.getElementById('hypernerf_image').style.opacity = "0";
+}
+
+// Initialize on page load
+hypernerf_stop();
+
